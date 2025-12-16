dbfddc1a806e647932ea4f401846ee7518be4323
jitendra kumar <k1995jitu@gmail.com>
2025-12-16 16:53:02 +0530

    Added practice code and new approach

--- src/main/java/org/dsa/array/MaxSubArraySum.java
+++ src/main/java/org/dsa/array/MaxSubArraySum.java
@@ -5,6 +5,8 @@ public class MaxSubArraySum {
     public static void main(String[] args){
         int[] nums = {-2, 1, -3, 4, -1, 2, 1, -5, 4};
         System.out.println("Max SubArray Sum: " + maxSubArray(nums));
+
+        maxSubArrayWithElements(nums);
     }
 
     private static int maxSubArray(int[] nums) {
@@ -16,5 +18,36 @@ public class MaxSubArraySum {
             maxSum = Math.max(maxSum,currentSum);
         }
         return maxSum;
+    }
+
+
+    private static void maxSubArrayWithElements(int[] nums) {
+
+        int currentSum = nums[0];
+        int maxSum = nums[0];
+
+        int start = 0, end = 0, tempStart = 0;
+
+        for (int i = 1; i < nums.length; i++) {
+
+            if (nums[i] > currentSum + nums[i]) {
+                currentSum = nums[i];
+                tempStart = i;
+            } else {
+                currentSum += nums[i];
+            }
+
+            if (currentSum > maxSum) {
+                maxSum = currentSum;
+                start = tempStart;
+                end = i;
+            }
+        }
+
+        System.out.println("Max Sum: " + maxSum);
+        System.out.print("Subarray: ");
+        for (int i = start; i <= end; i++) {
+            System.out.print(nums[i] + " ");
+        }
     }
 }
--- src/main/java/org/dsa/array/MoveZeros.java
+++ src/main/java/org/dsa/array/MoveZeros.java
@@ -5,7 +5,7 @@ import java.util.Arrays;
 public class MoveZeros {
     public static void main(String[] args){
         int []nums = {0,1,0,3,12};
-        moveZero(nums);
+        moveZeros2(nums);
         System.out.println(Arrays.toString(nums));
     }
     public static void moveZero(int[] arr){
@@ -19,5 +19,20 @@ public class MoveZeros {
        while(index<arr.length){
            arr[index++]=0;
        }
+    }
+
+    public static void moveZeros2(int[] arr) {
+        int j = 0;
+
+        for (int i = 0; i < arr.length; i++) {
+            if (arr[i] != 0) {
+                if (i != j) {       // swap only when required
+                    int temp = arr[i];
+                    arr[i] = arr[j];
+                    arr[j] = temp;
+                }
+                j++;
+            }
+        }
     }
 }
