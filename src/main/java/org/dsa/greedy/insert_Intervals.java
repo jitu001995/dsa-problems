6d661e2eb924a514e7d7addcbb260f7beb01571e
jitendra kumar <k1995jitu@gmail.com>
2025-12-21 11:14:37 +0530

    updated insert interval

--- src/main/java/org/dsa/greedy/insert_Intervals.java
+++ src/main/java/org/dsa/greedy/insert_Intervals.java
@@ -28,7 +28,7 @@ public class insert_Intervals {
              if(curr[1]>newInterval[0]){
                  result.add(curr);
              }else if(curr[0]>newInterval[1]){
-                 result.add(curr);
+                 result.add(newInterval);
                   newInterval = curr;
              }else{
                  newInterval[0] = Math.max(newInterval[0], curr[0]);
